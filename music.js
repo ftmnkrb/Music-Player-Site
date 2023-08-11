e38a5a30d359739f58024a71818b18f170678ec0
ftmnkrb <fkarabacak026@gmail.com>
2023-08-11 11:03:14 +0300

    class tanımlandı

---
+++ music.js
@@ -0,0 +1,12 @@
+class Music{
+     constructor(title, singer, img, file){
+          this.title = title;
+          this.singer = singer;
+          this.img = img;
+          this.file = file;
+     }
+
+     getName (){
+          return this.title + " " + this.singer;
+     }
+}
