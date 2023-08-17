e4a80a5e20d71c25aad6e757d912ffc4c4102029
ftmnkrb <fkarabacak026@gmail.com>
2023-08-17 11:14:10 +0300

    selectedMusic, isPlayinNow, displayMusicLİst

--- app.js
+++ app.js
@@ -10,12 +10,15 @@ const currentTime = document.querySelector("#current-time");
 const progressBar = document.querySelector("#progress-bar");
 const volume = document.querySelector("#volume");
 const volumeBar = document.querySelector("#volume-bar");
+const ul = document.querySelector("ul");
 
 const player = new MusicPlayer(musicList);
 
 window.addEventListener("load", () => {
     let music = player.getMusic();
     displayMusic(music);
+    displayMusicList(player.musicList);
+    isPlayingNow();
 });
 
 function displayMusic(music) {
@@ -44,6 +47,7 @@ function prevMusic() {
      let music = player.getMusic();
      displayMusic(music);
      playMusic();
+     isPlayingNow()
 }
 
 function nextMusic() {
@@ -51,16 +55,17 @@ function nextMusic() {
      let music = player.getMusic();
      displayMusic(music);
      playMusic();
+     isPlayingNow();
 }
 
 function pauseMusic() {
      container.classList.remove("playing");
-     play.classList = "bx bx-play-circle"
+     play.querySelector("i").classList = "bx bx-play-circle"
      audio.pause();
 }
 function playMusic() {
      container.classList.add("playing");
-     play.classList = "bx bx-pause-circle"
+     play.querySelector("i").classList = "bx bx-pause-circle"
      audio.play();
      
 }
@@ -92,7 +97,22 @@ progressBar.addEventListener("input", () => {
 let muteState = "sesli";
 
 volumeBar.addEventListener("input", (e) => {
-     console.log(e.target.value);
+     const value = e.target.value;
+     audio.volume = value / 100;
+     if(value == 0){
+          audio.muted = true;
+          muteState = "sessiz"
+          volume.classList.remove("bxs-volume-full"); // ben önce kaldırıp sonra istedğimiz classı ekledim ama direktte = ile ekleyebilirsin çünkü direk ona eşit olur.
+          volume.classList.add("bxs-volume-mute");
+          // volumeBar.value = 0;
+     }
+     else{
+          audio.muted = false;
+          muteState = "sesli"
+          volume.classList.remove("bxs-volume-mute");
+          volume.classList.add("bxs-volume-full");
+          // volumeBar.value = 100;
+     }
 });
 
 volume.addEventListener("click", () => {
@@ -112,3 +132,49 @@ volume.addEventListener("click", () => {
 
      }
 });
+
+const displayMusicList = (list) => {
+     for(let i=0; i<list.length; i++){
+          let liTag = `
+                    <li li-index=${i} onclick= "selectedMusic(this)" class="list-group-item d-flex justify-content-between align-items-center">
+                         <span>${list[i].getName()}</span>
+                         <span id="music-${i}" class="badge bg-primary rounded-pill">3:40</span>
+                         <audio class="music-${i}" src="mp3/${list[i].file}"></audio>
+                    </li>
+          `;
+
+          ul.insertAdjacentHTML("beforeend", liTag);
+
+
+          let liAudioDuration = ul.querySelector(`#music-${i}`);
+          let liAudioTag = ul.querySelector(`.music-${i}`);
+
+          liAudioTag.addEventListener("loadeddata", () => {
+               liAudioDuration.innerText = calculateTime(liAudioTag.duration);
+          })
+
+     }
+};
+
+const selectedMusic = (li) => {
+     player.index = li.getAttribute("li-index");
+     displayMusic(player.getMusic());
+     playMusic();
+     isPlayingNow();
+}
+
+const isPlayingNow= () => {
+     for(let li of ul.querySelectorAll("li")){
+          if(li.classList.contains("playing")){
+               li.classList.remove("playing")
+          };
+
+          if(li.getAttribute("li-index") == player.index) {
+               li.classList.add("playing")
+          }
+     }
+}
+
+audio.addEventListener("ended", () => {
+     nextMusic();
+} )
